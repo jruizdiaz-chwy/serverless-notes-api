168044656658ce8b42d93687e0d22ca25eb856f2
julianrd <julianrd@Julians-Mac-mini.local>
2019-03-06 10:18:25 -0300

    Making a mistake

--- create.js
+++ create.js
@@ -3,6 +3,8 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
+    gibberish.what;
+
     // Request body is passed in as a JSON encoded string in 'event.body'
     const data = JSON.parse(event.body);
 
